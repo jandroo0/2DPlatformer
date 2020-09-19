7054992ed5d36d933a00ec2e5d6702d43de117b0
jandroo0 <fortenacer@gmail.com>
2020-09-18 22:41:24 -0400

    packages

---
+++ Platformer/core/src/com/garcia/platformer/Screens/GameScreen.java
@@ -0,0 +1,43 @@
+package com.garcia.platformer.Screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.garcia.platformer.PlatformerGame;
+
+public class GameScreen extends ScreenTemplate {
+
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void render(float deltaTime) {
+        super.render(deltaTime);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
---
+++ Platformer/core/src/com/garcia/platformer/Screens/ScreenTemplate.java
@@ -0,0 +1,51 @@
+package com.garcia.platformer.Screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.garcia.platformer.PlatformerGame;
+
+public class ScreenTemplate implements Screen {
+
+    private PlatformerGame game;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+
+    private Music backroundMusic;
+
+    @Override
+    public void show() {
+        
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
